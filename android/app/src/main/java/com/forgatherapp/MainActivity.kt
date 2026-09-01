b91edb9d32113afebc28cc84d4dafdaffadcf8d2
H0ngju <hongju4385@gmail.com>
2026-09-01 16:23:41 +0900

    feat: Android 콜드 스타트 스플래시 테마 추가
    SplashTheme(윈도우 배경 = 스플래시 로고)을 추가해 콜드 스타트 시 표시하고,
    MainActivity.onCreate 에서 AppTheme 로 되돌려 스플래시 배경이 화면에 남지
    않도록 한다. Android 12+ 시스템 스플래시 배경도 브랜드 색으로 맞춘다.

--- android/app/src/main/java/com/forgatherapp/MainActivity.kt
+++ android/app/src/main/java/com/forgatherapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.forgatherapp
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  /**
+   * 콜드 스타트 시에는 SplashTheme(윈도우 배경 = 스플래시 로고)이 적용된다.
+   * RN 뷰가 올라오기 전에 앱 기본 테마로 되돌려, 스플래시 배경이 실제 화면에 남지 않도록 한다.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    setTheme(R.style.AppTheme)
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
